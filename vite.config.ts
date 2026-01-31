0561ce5f6ebb85644bcb171d38f68e5278c190b5
Omkar <omkargharat578@gmail.com>
2026-01-31 17:04:37 +0530

    feat: enhance SEO with meta tags, robots.txt, and sitemap generation

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import Sitemap from 'vite-plugin-sitemap';
 
 export default defineConfig(({ mode }) => {
   // Load env file based on `mode` in the current working directory.
@@ -7,7 +8,10 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, (process as any).cwd(), '');
 
   return {
-    plugins: [react()],
+    plugins: [
+      react(),
+      Sitemap({ hostname: 'https://zeronotes.app' })
+    ],
     build: {
       outDir: 'dist',
       target: 'esnext'
